54a59a186d9c0cdbd7d90d9c8eeccf93013cef52
nycsean67 <74220880+nycsean67@users.noreply.github.com>
2025-05-01 22:27:43 -0400

    Updates with a new destination renderer

--- app/components/SpecialOffer.js
+++ app/components/SpecialOffer.js
@@ -4,9 +4,9 @@ export default function SpecialOffer({ content }) {
 
   return (
     <div className="bg-white">
-      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div className="px-6 py-6 sm:px-6 sm:py-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
- {/* ----- HEADER & TEXT FROM CONTENTSTACK (I hope)----- */}
+ {/* ----- HEADER & TEXT FROM CONTENTSTACK (I----- */}
           <h5
             className="mt-8 text-2xl font-bold text-center tracking-widest text-neutral-700"
             {...content?.$?.single_line }

8117086657b688edc1cb88533c0a810616b29578
nycsean67 <74220880+nycsean67@users.noreply.github.com>
2025-05-01 22:21:53 -0400

    Initial commit

---
+++ app/components/NewDestinationDetail.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+export default function NewDestinationDetail({ content }) {
+
+  return (
+  
+
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+      {console.log('This log happens during render',content)}
+        {/* Image on the left */}
+      <div className="relative w-full h-64 md:h-96 rounded-lg overflow-hidden">
+      {console.log('Looking for the image', content.image.filename)}
+   {/*   <img src={content?.image?.filename} className="absolute inset-0 -z-10 h-full w-full object-cover object-center opacity-75"/> */}
+        <Image
+  //        src={content?.image?.filename}
+  //        alt={content?.image?.title}
+          //       src="/paris.jpg"
+          src="/london.jpeg"
+          fill
+          className="object-cover"
+          priority
+        />
+       </div>
+       {/* Text content on the right */}
+       <div className="space-y-4">
+        <h2 className="text-2xl md:text-3xl font-bold">{content.header}</h2>
+          <p className="text-gray-700">{content.description}  </p>
+      
+
+    </div>
+    </div>
+ 
+  );
+}
